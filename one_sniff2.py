73166f079767d41353a6bd7be51de7e558d85d08
kimsangwon <tkddnjs2014@naver.com>
2019-04-01 22:01:56 +0900

    nic

--- one_sniff2.py
+++ one_sniff2.py
@@ -1,26 +1,32 @@
-#!/usr/bin/python  
+#!/usr/bin/python
 # -*- coding: cp949 -*-b
 # -*- coding:utf-8 -*-
 
 
-from scapy.all import*  
-  
-def one_packet(packet):
-    src_add = packet[0][1].src  
-    #source address
-    dst_add = packet[0][1].dst 
-    #destination address 
-    proto = packet[0][1].proto  
-    #protocol type
-  
-    print ("protocol: %s, source: %s, destination: %s" %(proto, src_add, dst_add))
-  
-  
-def one_sniff(filte, iface):
-    sniff(iface=iface, filter=filte, prn=one_packet, count=20)
+from scapy.all import*
+import netifaces
 
-  
-if __name__ == '__main__': 
+def one_packet(packet):
+    src_add = packet[0][1].src
+    #source address
+    dst_add = packet[0][1].dst
+    #destination address
+    proto = packet[0][1].proto
+    #protocol type
+    
+    print ("protocol: %s, source: %s, destination: %s" %(proto, src_add, dst_add))
+
+
+def one_sniff(filte, iface):
+    sniff(iface=iface, filter=filte, prn=one_packet)
+
+
+if __name__ == '__main__':
+    b=netifaces.gateways()
+    print(b)
     conf.iface = input('input nic: ')
+    print ("ip")
+    print ("tcp")
+    print ("udp")
     filte = input('input protocol: ')
     one_sniff(filte, conf.iface)
